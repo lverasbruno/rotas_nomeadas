e04bbd46f2434b78b046a38c50cb50f0c00cb2d6
lverasbruno <123337999+lverasbruno@users.noreply.github.com>
2023-10-13 16:17:53 -0300

    enable web app

--- lib/presentation/games/widgets/game_list_empty.dart
+++ lib/presentation/games/widgets/game_list_empty.dart
@@ -5,7 +5,7 @@ class GameListEmpty extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text('Sem games para exibir!'),
     );
   }
